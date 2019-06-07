b7df13e05aaacf70f80dff0d8c4c498136d64584
partizaans <r.pourmeshki@gmail.com>
2019-06-07 23:46:49 +0430

    #1 'Use correct request method instead of test ones in urls.py'

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from core.views import FetchExams, VerifyExamView
 
 urlpatterns = [
-    path('', FetchExams.as_view({'get': 'post'})),
-    path('<pk>/', VerifyExamView.as_view({'get': 'post'}))
+    path('', FetchExams.as_view({'post': 'post'})),
+    path('<pk>/', VerifyExamView.as_view({'post': 'post'}))
 ]
